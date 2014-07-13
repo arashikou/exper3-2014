e0037428a4aedf1311b32eda980bd7967246a374
John W. Bruce <arashikou@gmail.com>
2014-07-13 12:30:56 -0700

    Fix Jumping
    There seems to be a bug around constantly accelerating the player into a
    surface in the current version of HaxeFlixel. I've made a note to
    investigate this further after EXPER3, but in the meantime, separating
    the player from the floor by a few pixels before jumping does the trick.

--- game1-jumpfight/source/Hero.hx
+++ game1-jumpfight/source/Hero.hx
@@ -4,6 +4,15 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColorUtil;
 
+// Constants that control player motion
+private class HeroPhysics
+{
+  public inline static var GRAVITY = 320;
+  public inline static var TERMINAL_VELOCITY = 500;
+  public inline static var JUMP_SPEED = TERMINAL_VELOCITY;
+  public inline static var JUMP_ABORT_SPEED = TERMINAL_VELOCITY / 10;
+}
+
 // Sprite for player character
 class Hero extends FlxSprite
 {
@@ -13,8 +22,8 @@ class Hero extends FlxSprite
 
     makeGraphic(16, 16, FlxColorUtil.getColor32(255, 0, 255, 0));
 
-    acceleration.y = 320;
-    maxVelocity.y = 200;
+    acceleration.y = HeroPhysics.GRAVITY;
+    maxVelocity.y = HeroPhysics.TERMINAL_VELOCITY;
     maxVelocity.x = 300;
     drag.y = 0;
     drag.x = 500;
@@ -35,11 +44,16 @@ class Hero extends FlxSprite
     // Handle jumping
     if (FlxG.keys.justPressed.UP) // && velocity.y == 0)
     {
-      velocity.y = -200;
+      // A jump is an instantaneous burst of upward speed.
+      velocity.y = -HeroPhysics.JUMP_SPEED;
+      // To work around a bug in the current version of HaxeFlixel, we need to
+      // prime the first few pixels of the jump.
+      y -= 2;
     }
-    else if (!FlxG.keys.pressed.UP && velocity.y < -50)
+    else if (!FlxG.keys.pressed.UP &&
+             velocity.y < -HeroPhysics.JUMP_ABORT_SPEED)
     {
-      velocity.y = -50;
+      velocity.y = -HeroPhysics.JUMP_ABORT_SPEED;
     }
   }
 }
