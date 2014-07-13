afe3462f652c08551c5a61055a31e9840fad0998
John W. Bruce <arashikou@gmail.com>
2014-07-12 21:48:16 -0700

    Add "Press Any Key" Text

--- game1-jumpfight/source/TitleState.hx
+++ game1-jumpfight/source/TitleState.hx
@@ -11,12 +11,21 @@ class TitleState extends FlxState
   {
     super.create();
 
-    var titleText = new FlxText(0, FlxG.height * 0.1);
+    var titleText = new FlxText();
     titleText.size = 100;
     titleText.font = "external_assets/fonts/Anton.ttf";
     titleText.text = "Jump Fight";
+    titleText.y = FlxG.height * 0.1;
     titleText.x = (FlxG.width - titleText.fieldWidth) / 2;
     add(titleText);
+
+    var startText = new FlxText();
+    startText.size = 20;
+    startText.font = "external_assets/fonts/Anton.ttf";
+    startText.text = "Press Any Key to Start";
+    startText.y = FlxG.height * 0.6;
+    startText.x = (FlxG.width - startText.fieldWidth) / 2;
+    add(startText);
   }
 
   override public function destroy():Void
