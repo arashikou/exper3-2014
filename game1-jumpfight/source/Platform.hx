08970f01be9a84e0182544db5368b6d8bbe4c9ce
John W. Bruce <arashikou@gmail.com>
2014-07-13 19:29:17 -0700

    Change Platform Constructor to Floats
    Although these will probably always be integers, the superclass uses
    floats, and there's no compelling reason to be more restrictive.
    (Whereas the additional restriction was causing trouble with
    calculations.)

--- game1-jumpfight/source/Platform.hx
+++ game1-jumpfight/source/Platform.hx
@@ -10,11 +10,11 @@ class Platform extends FlxSprite
 {
   public inline static var HEIGHT = 2;
 
-  public function new(start:Int, end:Int, altitude:Int)
+  public function new(start:Float, end:Float, altitude:Float)
   {
     super(start, FlxG.height - altitude - HEIGHT);
 
-    makeGraphic(end - start, HEIGHT, FlxColor.WHITE);
+    makeGraphic(Std.int(end - start), HEIGHT, FlxColor.WHITE);
 
     moves = false;
     immovable = true;
