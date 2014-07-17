0d7cd1bd94a577bfd5064cba98a79ef31c765cf5
John W. Bruce <arashikou@gmail.com>
2014-07-16 22:42:34 -0700

    Add Graphics for Platforms
    No more solid boxes!

--- game1-jumpfight/source/Platform.hx
+++ game1-jumpfight/source/Platform.hx
@@ -8,16 +8,41 @@ import flixel.util.FlxColor;
 // Sprite for a platform
 class Platform extends FlxSprite
 {
-  public inline static var HEIGHT = 2;
+  private var leftSprite:FlxSprite;
+  private var rightSprite:FlxSprite;
+  private var centerSprite:FlxSprite;
 
   public function new(start:Float, end:Float, altitude:Float)
   {
-    super(start, altitude - HEIGHT);
+    leftSprite = new FlxSprite(0, 0, "assets/images/Platform_L.png");
+    centerSprite = new FlxSprite(0, 0, "assets/images/Platform_C.png");
+    rightSprite = new FlxSprite(0, 0, "assets/images/Platform_R.png");
+    centerSprite.setGraphicSize(Std.int(end - start + 1 - leftSprite.width - rightSprite.width),
+                                Std.int(centerSprite.height));
+    centerSprite.updateHitbox();
 
-    makeGraphic(Std.int(end - start), HEIGHT, FlxColor.WHITE);
+    super(start, altitude - leftSprite.height);
+
+    makeGraphic(Std.int(end - start), Std.int(leftSprite.height), FlxColor.TRANSPARENT);
 
     moves = false;
     immovable = true;
     allowCollisions = UP;
+  }
+
+  override public function draw():Void
+  {
+    super.draw();
+
+    leftSprite.x = x;
+    leftSprite.y = y;
+    centerSprite.x = x + leftSprite.width;
+    centerSprite.y = y;
+    rightSprite.x = x + leftSprite.width + centerSprite.width;
+    rightSprite.y = y;
+
+    leftSprite.draw();
+    centerSprite.draw();
+    rightSprite.draw();
   }
 }
