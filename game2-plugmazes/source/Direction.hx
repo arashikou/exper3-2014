8e238b3eef9319847febf9cef8d49334b0cbce2f
John W. Bruce <arashikou@gmail.com>
2014-07-20 22:29:05 -0700

    Add shorthand Property to Direction

--- game2-plugmazes/source/Direction.hx
+++ game2-plugmazes/source/Direction.hx
@@ -7,4 +7,24 @@ abstract Direction(Int)
   public var RIGHT = 1;
   public var DOWN  = 2;
   public var LEFT  = 3;
+
+  public var shorthand(get, never):String;
+  inline public function get_shorthand():String
+  {
+    return switch (this)
+    {
+      case Direction.UP:
+        "U";
+      case Direction.DOWN:
+        "D";
+      case Direction.RIGHT:
+        "R";
+      case Direction.LEFT:
+        "L";
+      default:
+        // Haxe should detect that we have exhausted the enum, but for some
+        // reason it still demands a default.
+        throw "This should be impossible.";
+    }
+  }
 }
