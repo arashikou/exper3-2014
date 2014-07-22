97d5d5231396fca8ee078067d97b47260b6c9ed2
John W. Bruce <arashikou@gmail.com>
2014-07-22 00:06:46 -0700

    Fix null Track Crash
    intRanged is INCLUSIVE.

--- game2-plugmazes/source/BackgroundMusic.hx
+++ game2-plugmazes/source/BackgroundMusic.hx
@@ -15,13 +15,13 @@ class BackgroundMusic
 
   static public function play():Void
   {
-    currentTrack = FlxRandom.intRanged(0, _tracks.length);
+    currentTrack = FlxRandom.intRanged(0, _tracks.length - 1);
     playTrack();
   }
 
   static private function playTrack():Void
   {
-    currentTrack = FlxRandom.intRanged(0, _tracks.length, [currentTrack]);
+    currentTrack = FlxRandom.intRanged(0, _tracks.length - 1, [currentTrack]);
     var music = FlxG.sound.play("external_assets/music/" + _tracks[currentTrack] + ".ogg");
     music.onComplete = playTrack;
   }
