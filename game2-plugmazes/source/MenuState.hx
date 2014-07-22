13b06ed31deea8ea9edab1a12bea125ffabf81df
John W. Bruce <arashikou@gmail.com>
2014-07-22 00:35:59 -0700

    Add More Buttons to Faux Title Screen

--- game2-plugmazes/source/MenuState.hx
+++ game2-plugmazes/source/MenuState.hx
@@ -19,12 +19,27 @@ class MenuState extends FlxState
   {
     super.create();
 
-    var button = new TextButton("Music Credits");
-    button.clickCallback = function():Void
+    var leftArrow = new Button("assets/images/LeftArrowButton.png", 30, 60);
+    leftArrow.x = FlxG.width / 2 - 50 - leftArrow.width;
+    add(leftArrow);
+
+    var rightArrow = new Button("assets/images/RightArrowButton.png", 30, 60);
+    rightArrow.x = FlxG.width / 2 + 50;
+    add(rightArrow);
+
+    var playButton = new TextButton("Play");
+    playButton.x = (FlxG.width - playButton.width) / 2;
+    playButton.y = (FlxG.height - playButton.height) / 2;
+    add(playButton);
+
+    var musicButton = new TextButton("Music Credits");
+    musicButton.x = (FlxG.width - musicButton.width) / 2;
+    musicButton.y = FlxG.height - musicButton.height;
+    musicButton.clickCallback = function():Void
     {
       FlxG.switchState(new MusicCreditsState());
     };
-    add(button);
+    add(musicButton);
   }
 
   /**
