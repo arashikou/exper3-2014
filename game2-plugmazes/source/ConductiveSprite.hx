3f89c0891439addbd4b14ee329f33978f42919ea
John W. Bruce <arashikou@gmail.com>
2014-07-25 02:16:35 -0700

    Make ConductiveSprites Glow when Powered

--- game2-plugmazes/source/ConductiveSprite.hx
+++ game2-plugmazes/source/ConductiveSprite.hx
@@ -1,10 +1,20 @@
 package;
 
 import flixel.FlxSprite;
+import flixel.util.FlxColor;
 
 class ConductiveSprite extends FlxSprite implements Powerable implements Connectable
 {
-  private var _source: Powerable;
+  private var _source:Powerable;
+  private var _poweredBG:FlxSprite;
+
+  public function new()
+  {
+    super();
+
+    _poweredBG = new FlxSprite();
+    _poweredBG.makeGraphic(Constants.CELL_SIZE, Constants.CELL_SIZE, FlxColor.YELLOW);
+  }
 
   public function isPowered():Bool
   {
@@ -18,5 +28,17 @@ class ConductiveSprite extends FlxSprite implements Powerable implements Connect
   public function connectTo(source:Powerable):Void
   {
     _source = source;
+  }
+
+  override public function draw():Void
+  {
+    if (isPowered())
+    {
+      _poweredBG.x = x;
+      _poweredBG.y = y;
+      _poweredBG.draw();
+    }
+
+    super.draw();
   }
 }
