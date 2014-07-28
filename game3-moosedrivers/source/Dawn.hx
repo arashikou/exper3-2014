7cb94864094a6d8189f26ca5a1765d82a4896919
John W. Bruce <arashikou@gmail.com>
2014-07-27 18:49:18 -0700

    Make Dawn Save the State

--- game3-moosedrivers/source/Dawn.hx
+++ game3-moosedrivers/source/Dawn.hx
@@ -1,11 +1,23 @@
 package;
 
 import flixel.FlxState;
+import flixel.util.FlxSave;
 
 class Dawn extends FlxState
 {
+  private var _state:GameState;
+
   public function new(state:GameState)
   {
     super();
+    _state = state;
+  }
+
+  override public function create():Void
+  {
+    var saveData = new FlxSave();
+    saveData.bind("the-only-save");
+    _state.save(saveData);
+    saveData.close();
   }
 }
