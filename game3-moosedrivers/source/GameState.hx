a2dedf30dcd53fc09aa5b078601f111a71dfb522
John W. Bruce <arashikou@gmail.com>
2014-07-27 18:48:51 -0700

    Add Trivial Saving and Loading Capability

--- game3-moosedrivers/source/GameState.hx
+++ game3-moosedrivers/source/GameState.hx
@@ -8,8 +8,22 @@ class GameState
   {
   }
 
-  static public function load(save:FlxSave):GameState
+  static public function load(saveData:FlxSave):GameState
   {
-    return null;
+    var version:Int = saveData.data.version;
+    var serializedState:String = saveData.data.serializedState;
+    return switch (version)
+    {
+      case 1:
+        new GameState();
+      default:
+        throw "Unrecognized Save Version";
+    }
+  }
+
+  public function save(saveData:FlxSave):Void
+  {
+    saveData.data.version = 1;
+    saveData.data.serializedState = "";
   }
 }
