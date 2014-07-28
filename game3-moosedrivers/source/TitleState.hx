bd95b6af2ab3d12a8584050e0a9f945db7b40810
John W. Bruce <arashikou@gmail.com>
2014-07-27 18:11:16 -0700

    Add Buttons to Title Screen

--- game3-moosedrivers/source/TitleState.hx
+++ game3-moosedrivers/source/TitleState.hx
@@ -19,5 +19,15 @@ class TitleState extends FlxState
     title.y = 96;
     title.x = Std.int((FlxG.width - title.fieldWidth) / 2);
     add(title);
+
+    var newButton = new TextButton("New Game");
+    newButton.y = 350;
+    newButton.x = Std.int((FlxG.width - newButton.width) / 2);
+    add(newButton);
+
+    var loadButton = new TextButton("Load Game");
+    loadButton.y = 475;
+    loadButton.x = Std.int((FlxG.width - loadButton.width) / 2);
+    add(loadButton);
   }
 }
