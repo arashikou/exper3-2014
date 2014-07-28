0db9da8610a5d447660784a0d9bfe3be64fe6c70
John W. Bruce <arashikou@gmail.com>
2014-07-27 19:03:36 -0700

    Invent Some Town Names

--- game3-moosedrivers/source/Constants.hx
+++ game3-moosedrivers/source/Constants.hx
@@ -3,4 +3,19 @@ package;
 class Constants
 {
   inline static public var saveName = "the-only-save";
+
+  inline static public var townNames = [
+    "Ottawa",
+    "Vancouver",
+    "Calgary",
+    "Winnipeg",
+    "Regina",
+    "St. John's",
+    "Istanbul",
+    "Chicago",
+    "Milwaukee",
+    "New York City",
+    "Tucson",
+    "Beijing"
+  ];
 }
