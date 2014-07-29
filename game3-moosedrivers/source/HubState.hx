4ad655da8b0a37446b6558dd5529097eb0f73420
John W. Bruce <arashikou@gmail.com>
2014-07-29 00:13:44 -0700

    Display More Stats on Hub

--- game3-moosedrivers/source/HubState.hx
+++ game3-moosedrivers/source/HubState.hx
@@ -5,6 +5,11 @@ import flixel.util.FlxColor;
 class HubState extends SimulationState
 {
   private var _moosepowerReadout:MooseText;
+  private var _mooseReadout:MooseText;
+  private var _energyReadout:MooseText;
+  private var _humanFoodReadout:MooseText;
+  private var _mooseFeedReadout:MooseText;
+  private var _reverseOdometer:MooseText;
 
   override public function create():Void
   {
@@ -12,10 +17,40 @@ class HubState extends SimulationState
     _moosepowerReadout.x = 10;
     _moosepowerReadout.y = 10;
     add(_moosepowerReadout);
+
+    _mooseReadout = new MooseText(18, FlxColor.WHITE);
+    _mooseReadout.x = 10;
+    _mooseReadout.y = 44;
+    add(_mooseReadout);
+
+    _energyReadout = new MooseText(18, FlxColor.WHITE);
+    _energyReadout.x = 10;
+    _energyReadout.y = 64;
+    add(_energyReadout);
+
+    _humanFoodReadout = new MooseText(18, FlxColor.WHITE);
+    _humanFoodReadout.x = 10;
+    _humanFoodReadout.y = 84;
+    add(_humanFoodReadout);
+
+    _mooseFeedReadout = new MooseText(18, FlxColor.WHITE);
+    _mooseFeedReadout.x = 10;
+    _mooseFeedReadout.y = 104;
+    add(_mooseFeedReadout);
+
+    _reverseOdometer = new MooseText(32, FlxColor.WHITE);
+    _reverseOdometer.x = 10;
+    _reverseOdometer.y = 124;
+    add(_reverseOdometer);
   }
 
   override public function update():Void
   {
-    _moosepowerReadout.text = "Moosepower: " + _status.moosepower;
+    _moosepowerReadout.text = _status.moosepower + " Moosepower";
+    _mooseReadout.text = "Herd: " + _status.mooseCount + " Moose";
+    _energyReadout.text = "Batteries: " + _status.mooseCount + " Energy";
+    _humanFoodReadout.text = "Human Food: " + _status.humanFoodLevel + " Meals";
+    _mooseFeedReadout.text = "Moose Feed: " + _status.mooseFeedLevel + " Sachets";
+    _reverseOdometer.text = _status.distanceToNextTown + " km to " + _status.nameOfNextTown;
   }
 }
