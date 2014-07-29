cada33743ad90cf1511305942b54531d3d524208
John W. Bruce <arashikou@gmail.com>
2014-07-28 23:01:22 -0700

    Add Dawn Animation to DawnState
    After which, we go to the hub state.

--- game3-moosedrivers/source/DawnState.hx
+++ game3-moosedrivers/source/DawnState.hx
@@ -1,5 +1,8 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxSave;
 
 class DawnState extends SimulationState
@@ -10,5 +13,23 @@ class DawnState extends SimulationState
     saveData.bind(Constants.saveName);
     _status.save(saveData);
     saveData.close();
+
+    var night = new FlxSprite("assets/images/Night.png");
+    night.x = (FlxG.width - night.width) / 2;
+    night.y = (FlxG.height - night.height) / 2;
+    add(night);
+
+    var dawn = new FlxSprite("assets/images/Dawn.png");
+    dawn.x = night.x;
+    dawn.y = night.y;
+    dawn.alpha = 0;
+    add(dawn);
+
+    var tween = FlxTween.tween(dawn, {alpha: 1}, 6, {complete:tweenDone});
+  }
+
+  private function tweenDone(Tween:FlxTween):Void
+  {
+    FlxG.switchState(new HubState(_status));
   }
 }
