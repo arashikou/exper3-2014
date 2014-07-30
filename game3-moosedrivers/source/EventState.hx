fca8fef695b7b423a92edc022357ae317f59b169
John W. Bruce <arashikou@gmail.com>
2014-07-29 21:56:21 -0700

    Make EventState Use Events

--- game3-moosedrivers/source/EventState.hx
+++ game3-moosedrivers/source/EventState.hx
@@ -4,13 +4,11 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 
-typedef EventFunction = SimulationStatus -> String;
-
 class EventState extends SimulationState
 {
-  private var _event:EventFunction;
+  private var _event:Event;
 
-  public function new(status:SimulationStatus, event:EventFunction)
+  public function new(status:SimulationStatus, event:Event)
   {
     super(status);
     _event = event;
@@ -24,18 +22,39 @@ class EventState extends SimulationState
     add(image);
 
     var text = new MooseText(16, FlxColor.WHITE);
-    text.text = _event(_status);
+    text.text = _event.action(_status);
     text.x = 10;
     text.y = image.y + image.height + 10;
     add(text);
 
-    var continueButton = new TextButton("Continue");
-    continueButton.x = FlxG.width - 10 - continueButton.width;
-    continueButton.y = FlxG.height - 10 - continueButton.height;
-    continueButton.clickCallback = function():Void
+    if (_event.options.length == 0)
     {
-      FlxG.switchState(new HubState(_status));
-    };
-    add(continueButton);
+      var continueButton = new TextButton("Continue");
+      continueButton.x = FlxG.width - 10 - continueButton.width;
+      continueButton.y = FlxG.height - 10 - continueButton.height;
+      continueButton.clickCallback = function():Void
+      {
+        FlxG.switchState(new HubState(_status));
+      };
+      add(continueButton);
+    }
+    else
+    {
+      for(index in 0..._event.options.length)
+      {
+        var option = _event.options[index];
+        if (option != null)
+        {
+          var button = new TextButton(option.text);
+          button.x = (10 * (index + 1)) + (button.width * index);
+          button.y = FlxG.height - 10 - button.height;
+          button.clickCallback = function():Void
+          {
+            FlxG.switchState(new EventState(_status, option.next));
+          };
+          add(button);
+        }
+      }
+    }
   }
 }
