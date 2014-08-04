b0f665e030eaf084438c62975ef9beb17e5ac783
John W. Bruce <arashikou@gmail.com>
2014-08-04 01:27:52 -0700

    Remove Y Movement Entirely
    It's just a distraction at this point.

--- game4-houseofmysteries/source/Player.hx
+++ game4-houseofmysteries/source/Player.hx
@@ -4,17 +4,13 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 
-using flixel.util.FlxSpriteUtil;
-
 class Player extends FlxSprite
 {
-  inline static private var Y_SPEED = 400;
-
   public function new()
   {
     super();
     makeGraphic(32, 32, FlxColor.WHEAT);
-    y = FlxG.height - height;
+    y = FlxG.height - height - 20;
   }
 
   override public function update():Void
@@ -25,12 +21,5 @@ class Player extends FlxSprite
     if (FlxG.keys.pressed.D) xMove += 1;
     if (FlxG.keys.pressed.A) xMove -= 1;
     x = xMove * (FlxG.width / 4) - width / 2;
-
-    var yMove = 0;
-    if (FlxG.keys.pressed.S) yMove += 1;
-    if (FlxG.keys.pressed.W) yMove -= 1;
-    y += yMove * FlxG.elapsed * Y_SPEED;
-
-    bound();
   }
 }
