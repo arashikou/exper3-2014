3b19772292da1cb0e310404e850d0e583a383106
John W. Bruce <arashikou@gmail.com>
2014-08-03 21:17:28 -0700

    Use Targetting Cursor

--- game4-houseofmysteries/source/FlightState.hx
+++ game4-houseofmysteries/source/FlightState.hx
@@ -10,8 +10,16 @@ class FlightState extends FlxState
   override public function create():Void
   {
     super.create();
+    FlxG.mouse.load("assets/images/Target.png", 1, -8, -8);
+
     player = new Player();
     add(player);
+  }
+
+  override public function destroy():Void
+  {
+    super.destroy();
+    FlxG.mouse.load();
   }
 
   override public function update():Void
