9e43c21884c5cb6b5604ce1452471a6bccbe4d95
John W. Bruce <arashikou@gmail.com>
2014-08-03 19:42:20 -0700

    Change PlayState to FlightState

---
+++ game4-houseofmysteries/source/FlightState.hx
@@ -0,0 +1,22 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+
+class FlightState extends FlxState
+{
+  override public function create():Void
+  {
+    super.create();
+  }
+
+  override public function destroy():Void
+  {
+    super.destroy();
+  }
+
+  override public function update():Void
+  {
+    super.update();
+  }
+}
