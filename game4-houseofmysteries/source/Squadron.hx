4583a73a1b801da72ea144aacf39e83f662dbd12
John W. Bruce <arashikou@gmail.com>
2014-08-09 17:49:20 -0700

    Add Squadrons of Four

--- game4-houseofmysteries/source/Squadron.hx
+++ game4-houseofmysteries/source/Squadron.hx
@@ -28,6 +28,25 @@ class Squadron extends FlxTypedGroup<Apparition>
         members[1].y = 100;
         members[2].x = (FlxG.width - members[2].width) / 2;
         members[2].y = 200;
+      case 4:
+        var primarySubject = Apparition.Random.subject();
+        var secondarySubject = Apparition.Random.subject(primarySubject);
+
+        add(new Apparition(primarySubject, Apparition.Random.form()));
+        add(new Apparition(secondarySubject, Apparition.Random.form()));
+        add(new Apparition(primarySubject, Apparition.Random.form(members[0].form)));
+        add(new Apparition(secondarySubject, Apparition.Random.form(members[1].form)));
+
+        FlxRandom.shuffleArray(members, members.length * 4);
+
+        members[0].x = 100;
+        members[0].y = 100;
+        members[1].x = FlxG.width - 100 - members[1].width;
+        members[1].y = 100;
+        members[2].x = 100;
+        members[2].y = 200;
+        members[3].x = FlxG.width - 100 - members[3].width;
+        members[3].y = 200;
       default:
         throw "Unusable size!";
     }
