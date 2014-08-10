25803480c1e382c12b02aac21978576811cd29f3
John W. Bruce <arashikou@gmail.com>
2014-08-09 18:41:30 -0700

    Make RotatingCross Live Up to Its Name

--- game4-houseofmysteries/source/BulletStrategy.hx
+++ game4-houseofmysteries/source/BulletStrategy.hx
@@ -22,26 +22,30 @@ class BulletStrategy
 
 class RotatingCross extends BulletStrategy
 {
-  private var angle:Float;
+  private var _angle:Float;
 
   public function new(group:Bullet.Group, source:FlxObject)
   {
     super(group, source);
-    angle = 0;
+    _angle = 0;
   }
 
   override public function fire():Void
   {
-    var bullet = _group.getFirstDead();
-    bullet.revive();
-    bullet.x = _source.x + (_source.width - bullet.width) / 2;
-    bullet.y = _source.y + (_source.height - bullet.height) / 2;
-    // -90 to deal with the different origins of this and velocityFromAngle.
-    bullet.angle = angle - 90;
-    var v = FlxVelocity.velocityFromAngle(angle, 100);
-    bullet.velocity.x = v.x;
-    bullet.velocity.y = v.y;
+    for (addon in [0, 90, 180, 270])
+    {
+      var bullet = _group.getFirstDead();
+      bullet.revive();
+      bullet.x = _source.x + (_source.width - bullet.width) / 2;
+      bullet.y = _source.y + (_source.height - bullet.height) / 2;
+      var angle = _angle + addon;
+      // -90 to deal with the different origins of this and velocityFromAngle.
+      bullet.angle = angle - 90;
+      var v = FlxVelocity.velocityFromAngle(angle, 100);
+      bullet.velocity.x = v.x;
+      bullet.velocity.y = v.y;
+    }
 
-    angle += 10;
+    _angle += 10;
   }
 }
