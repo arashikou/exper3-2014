1a1bf8e7aa3ecdbb09dcf9d672faedb11f92a5d6
John W. Bruce <arashikou@gmail.com>
2014-08-09 18:50:08 -0700

    Add Bullet Group to FlightState

--- game4-houseofmysteries/source/FlightState.hx
+++ game4-houseofmysteries/source/FlightState.hx
@@ -6,12 +6,16 @@ import flixel.FlxState;
 class FlightState extends FlxState
 {
   private var _player:Player;
+  private var _bullets:Bullet.Group;
 
   override public function create():Void
   {
     super.create();
     FlxG.mouse.load("assets/images/Target.png", 1, -8, -8);
     add(new CrazyBackground());
+
+    _bullets = new Bullet.Group();
+    add(_bullets);
 
     _player = new Player();
     add(_player);
@@ -29,5 +33,13 @@ class FlightState extends FlxState
   override public function update():Void
   {
     super.update();
+    _bullets.forEachAlive(function(bullet:Bullet):Void
+    {
+      if (bullet.x < -100 || bullet.x > FlxG.width + 100 ||
+          bullet.y < -100 || bullet.y > FlxG.height + 100)
+      {
+        bullet.kill();
+      }
+    });
   }
 }
