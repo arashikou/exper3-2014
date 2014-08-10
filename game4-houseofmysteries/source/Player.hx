411dde5e5fa46e84de01a43cd8759bc3ff5c5844
John W. Bruce <arashikou@gmail.com>
2014-08-09 20:05:48 -0700

    Revert All Recent Changes to Player
    Reverts the player to their state in 8fc81a6.

--- game4-houseofmysteries/source/Player.hx
+++ game4-houseofmysteries/source/Player.hx
@@ -4,22 +4,46 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 
+using flixel.util.FlxSpriteUtil;
+
 class Player extends FlxSprite
 {
+  public var heart(default,null):FlxSprite;
+
+  inline static private var LOW_SPEED = 100;
+  inline static private var HIGH_SPEED = 500;
+
   public function new()
   {
     super();
     makeGraphic(32, 32, FlxColor.WHEAT);
-    y = FlxG.height - height - 20;
+    heart = new FlxSprite();
+    heart.makeGraphic(2, 2, FlxColor.RED);
   }
 
   override public function update():Void
   {
     super.update();
 
-    var xMove = 2;
+    var xMove = 0;
     if (FlxG.keys.pressed.D) xMove += 1;
     if (FlxG.keys.pressed.A) xMove -= 1;
-    x = xMove * (FlxG.width / 4) - width / 2;
+    var yMove = 0;
+    if (FlxG.keys.pressed.S) yMove += 1;
+    if (FlxG.keys.pressed.W) yMove -= 1;
+    var speed = FlxG.keys.pressed.SHIFT ? LOW_SPEED : HIGH_SPEED;
+    x += xMove * FlxG.elapsed * speed;
+    y += yMove * FlxG.elapsed * speed;
+    bound();
+
+    heart.x = x + (width - heart.width) / 2;
+    heart.y = y + (height - heart.height) / 2;
+    heart.update();
+  }
+
+  override public function draw():Void
+  {
+    super.draw();
+    heart.draw();
   }
 }
